0dda2ed0fd69341065fbc3b35bc168a2154e5796
Ovuoba Chiagozie <ovuobachiagozie@gmail.com>
2023-03-29 02:56:53 +0100

    finished footer component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,16 @@
 import Header from "../MainPageComponenets/Header";
+import Footer from "../MainPageComponenets/Footer";
 import "../homepage.css";
 const HomePage = () => {
-  return <Header />;
+  return( 
+  
+    <div>
+      <Header />
+      <Footer />
+    </div>
+
+
+  );
 };
 
 export default HomePage;
