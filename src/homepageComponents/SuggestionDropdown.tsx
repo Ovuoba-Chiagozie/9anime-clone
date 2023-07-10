f240a788527800877fad3fa0b3a4e4383ee7543c
Ovuoba Chiagozie <ovuobachiagozie@gmail.com>
2023-07-10 12:46:03 +0100

    fixed bugs from images not loading and malfunctioning search

--- src/homepageComponents/SuggestionDropdown.tsx
+++ src/homepageComponents/SuggestionDropdown.tsx
@@ -1,193 +1,155 @@
-import React, { ReactElement, ReactNode } from 'react'
+import React, { ReactElement, ReactNode } from "react";
 
 type Cardprops = {
-
-  key: number,
-  image: string,
-  title: string
-  rating: string | string[]
-  score: number
-  type: string
-  aired: string | string[]
-  color: boolean
-
-}
+  key: number;
+  image: string;
+  title: string;
+  rating: string | string[];
+  score: number;
+  type: string;
+  aired: string | string[];
+  color: boolean;
+};
 
 let Card = (prop: Cardprops) => {
- 
   let styles = {
-
     backgroundColor: prop.color ? "#141414" : "#1c1c1c",
-
-  }
+  };
 
   return (
-
     <div style={styles}>
-      <a href="" className='searchCard'>
-      <img  src={prop.image} alt="" />
-      <div className='searchCard--info'>
-        <div className='searchCard--title'>{prop.title}</div>
-        <div className='searchCard--extrainfo'>
-          {prop.rating !== "" && <span className='searchCard--rating'>{prop.rating}</span>}
-          <span className='dot'>&bull;</span>
-          <span className='searchCard--score'>
-            <i className="fa-solid fa-star"></i>
-            {prop.score}
-          </span>
-          <span className='dot'>&bull;</span>
-          <span className='type--date'>
-            {prop.type}
-          </span>
-          <span className='dot'>&bull;</span>
-          <span className='type--date'>
-          {prop.aired !=="" && <span>{prop.aired}</span>}
-          </span>
+      <a href="" className="searchCard">
+        <img src={prop.image} alt="" />
+        <div className="searchCard--info">
+          <div className="searchCard--title">{prop.title}</div>
+          <div className="searchCard--extrainfo">
+            {prop.rating !== "" && (
+              <span className="searchCard--rating">{prop.rating}</span>
+            )}
+            <span className="dot">&bull;</span>
+            <span className="searchCard--score">
+              <i className="fa-solid fa-star"></i>
+              {prop.score}
+            </span>
+            <span className="dot">&bull;</span>
+            <span className="type--date">{prop.type}</span>
+            <span className="dot">&bull;</span>
+            <span className="type--date">
+              {prop.aired !== "" && <span>{prop.aired}</span>}
+            </span>
           </div>
-      </div>
+        </div>
       </a>
-      </div>
-
-  )
-
-}
+    </div>
+  );
+};
 
 let NoResult = () => {
+  let viewAll = document.querySelector(".view--all") as HTMLDivElement | null;
 
-  let viewAll = document.querySelector('.view--all') as HTMLDivElement | null
-
-  if(viewAll != null) {
-
-            viewAll.classList.remove('display')
-
+  if (viewAll != null) {
+    viewAll.classList.remove("display");
   }
 
-  return (
-
-    <div id='no--result'>There is no result</div>
-
-  )
-
-}
+  return <div id="no--result">There is no result</div>;
+};
 
 type dropdownSuggestion = {
-
-  anime: string
-
-}
+  anime: string;
+};
 
 let SuggestionDropdown = (prop: dropdownSuggestion) => {
-      
-    let [animeImages, setAnimeImages] = React.useState<ReactNode>([])
+  // try {
+  console.log('prop',prop)
+  let [animeImages, setAnimeImages] = React.useState<ReactNode>([]);
 
-    type Divelement = HTMLDivElement | null
-    
-   
-    React.useEffect(() => {
-      if(prop.anime.length > 1) {
-      const url: string = `https://api.jikan.moe/v4/anime?q=*${prop.anime}&limit=5*`
+  type Divelement = HTMLDivElement | null;
 
-      // type arrayData = {
+  React.useEffect(() => {
+    try {
+      if (prop.anime.length > 1) {
+        const url: string = `https://api.jikan.moe/v4/anime?q=${prop.anime}&limit=5`;
 
-      //   mal_id: number,
-      //   images: {jpg:{image_url: string}}
-      //   title: string
-      //   rating: string
-      //   score: number
-      //   type: string
-      //   aired:{string: string}
-  
-      // }
+        type Anime = {
+          data: {
+            mal_id: number;
+            images: { jpg: { image_url: string } };
+            title: string;
+            rating: string;
+            score: number;
+            type: string;
+            aired: { string: string };
+          }[];
+        };
 
-      type Anime = {
+        fetch(url)
+          .then((response) => response.json())
+          .then((anime: Anime) => {
+            console.log(anime, "mad");
+            if (anime.data.length > 0) {
+              setAnimeImages(
+                anime.data.map((dataArray, index: number) => {
+                  let id = dataArray.mal_id;
+                  let poster = dataArray.images.jpg.image_url;
+                  let title = dataArray.title;
+                  let rating = dataArray.rating;
+                  let score = dataArray.score;
+                  let type = dataArray.type;
+                  let aired = dataArray.aired.string;
+                  let cards = (
+                    <Card
+                      key={id}
+                      image={poster}
+                      title={title}
+                      rating={rating ? rating.split(" - ", 1) : ""}
+                      score={score}
+                      type={type ? type.toUpperCase() : ""}
+                      aired={aired ? aired.split(" to", 1) : ""}
+                      color={index % 2 === 0 ? true : false}
+                    />
+                  );
 
-        data: {
-        
-        mal_id: number,
-        images: {jpg:{image_url: string}}
-        title: string
-        rating: string
-        score: number
-        type: string
-        aired:{string: string}
-       
-        }[]
+                  if (anime.data.length == 0) {
+                    let viewAll = document.querySelector(
+                      ".view--all"
+                    ) as Divelement;
+                    if (viewAll != null) {
+                      viewAll.classList.add("display");
+                    }
+                  }
 
-      }
-
-      fetch(url)
-      .then(response => response.json())
-      .then((anime: Anime) => {
-        console.log(anime,"mad")
-        if (anime.data.length > 0) {
-
-        setAnimeImages(anime.data.map((dataArray,index: number) => {
-          let id = dataArray.mal_id
-          let poster = dataArray.images.jpg.image_url
-          let title = dataArray.title
-          let rating = dataArray.rating
-          let score = dataArray.score
-          let type = dataArray.type
-          let aired = dataArray.aired.string
-          let cards = <Card 
-                         key={id}
-                         image = {poster}
-                         title = {title}
-                         rating = {rating ? rating.split(' - ',1) : ""}
-                         score = {score}
-                         type = {type ? type.toUpperCase() : ""}
-                         aired = {aired ? aired.split(' to',1) : ""}
-                         color ={index % 2 === 0 ? true : false}
-                         />
-
-          if (anime.data.length == 0) {
-            let viewAll = document.querySelector('.view--all') as Divelement
-            if(viewAll != null) {
-            viewAll.classList.add('display')
+                  return cards;
+                })
+              );
+            } else {
+              setAnimeImages(<NoResult />);
             }
-          }
+            console.log("anime", prop.anime);
 
-          return cards
+            let dropDownwrapper =
+              document.getElementsByClassName("dropDown--wrapper");
+            let footer = document.querySelector(
+              ".searchbox--footer"
+            ) as Divelement;
 
-        }))
+            dropDownwrapper[0].classList.remove("display--height");
+
+            if (footer != null) {
+              footer.classList.add("display--footer");
+            }
+
+            setTimeout(() => {
+              dropDownwrapper[0].classList.add("display--height");
+            }, 3);
+          });
       }
-
-      else {
-        setAnimeImages(<NoResult />)
-      }
-      console.log('anime',prop.anime)
-      
-
-      let dropDownwrapper = document.getElementsByClassName('dropDown--wrapper')
-      let footer = document.querySelector('.searchbox--footer') as Divelement
-     
-      dropDownwrapper[0].classList.remove('display--height')
-
-      if (footer != null) {
-
-      footer.classList.add('display--footer')
-
-      }
-
-      setTimeout(() => {
-
-        dropDownwrapper[0].classList.add('display--height')
-        
-      },3 );
-      })
-     
+    } catch (error: any) {
+      console.log(error);
+      console.log(error.message);
     }
-    },[prop.anime])
-    
-    
+  }, [prop.anime]);
 
-    return (
-        
-        <div className='dropDown--wrapper'>{animeImages}</div>
-        
+  return <div className="dropDown--wrapper">{animeImages}</div>;
+};
 
-    )
-
-  }
-
-  export default SuggestionDropdown
+export default SuggestionDropdown;
